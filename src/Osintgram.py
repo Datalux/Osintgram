65eb41f44205ad767b9e0c59836e27abf32d5b9b
Datalux <giuseppe.criscione@gmail.com>
2020-09-16 12:11:36 +0200

    feat: set itself as target

--- src/Osintgram.py
+++ src/Osintgram.py
@@ -1044,6 +1044,8 @@ class Osintgram:
             # print('SAVED: {0!s}'.format(new_settings_file))
 
     def check_following(self):
+        if self.target_id == self.api.authenticated_user_id:
+            return True
         endpoint = 'users/{user_id!s}/full_detail_info/'.format(**{'user_id': self.target_id})
         return self.api._call_api(endpoint)['user_detail']['user']['friendship_status']['following']
 
