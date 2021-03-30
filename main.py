e393477009287691e8e1fa6f1b7ac40eebb77b3b
Md Faizan Ahmad <ahmadfaizan965@gmail.com>
2021-03-30 20:43:07 +0530

    Code Cleanup & Ascii Art Improved

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 from src.Osintgram import Osintgram
 import argparse
 from src import printcolors as pc
+from src import ascii_art
 import sys
 import signal
 import readline
@@ -15,13 +16,7 @@ commands = ["quit", "exit", "list", "help", "addrs", "captions", "comments", "fo
 
 
 def printlogo():
-    pc.printout("________         .__        __                               \n", pc.YELLOW)
-    pc.printout("\_____  \   _____|__| _____/  |_  ________________    _____  \n", pc.YELLOW)
-    pc.printout(" /   |   \ /  ___/  |/    \   __\/ ___\_  __ \__  \  /     \ \n", pc.YELLOW)
-    pc.printout("/    |    \\\___ \|  |   |  \  | / /_/  >  | \// __ \|  Y Y  \\\n", pc.YELLOW)
-    pc.printout("\_______  /____  >__|___|  /__| \___  /|__|  (____  /__|_|  /\n", pc.YELLOW)
-    pc.printout("        \/     \/        \/    /_____/            \/      \/ \n", pc.YELLOW)
-    print('\n')
+    pc.printout(ascii_art, pc.YELLOW)
     pc.printout("Version 1.0.1 - Developed by Giuseppe Criscione\n\n", pc.YELLOW)
     pc.printout("Type 'list' to show all allowed commands\n")
     pc.printout("Type 'FILE=y' to save results to files like '<target username>_<command>.txt (deafult is disabled)'\n")
