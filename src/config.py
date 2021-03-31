3efa111b069e51c8116133ee65666675eb7b5532
Giuseppe Criscione <giuseppe.criscione@gmail.com>
2021-03-31 20:07:26 +0200

    Update src/config.py
    Co-authored-by: Christian Clauss <cclauss@me.com>

--- src/config.py
+++ src/config.py
@@ -7,8 +7,7 @@ try:
     config = configparser.ConfigParser(interpolation=None)
     config.read("config/credentials.ini")
 except FileNotFoundError:
-    pc.printout("Error: file \"config/credentials.ini\" not found!", pc.RED)
-    pc.printout("\n")
+    pc.printout('Error: file "config/credentials.ini" not found!\n', pc.RED)
     sys.exit(0)
 except Exception as e:
     pc.printout("Error: {}".format(e), pc.RED)
