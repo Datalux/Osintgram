de0195ad56734510d3e945157ebacd8646067980
shadow00 <4933508+shadow00@users.noreply.github.com>
2021-03-31 20:41:55 +0200

    Better formatting for error printouts in config.py

--- src/config.py
+++ src/config.py
@@ -10,22 +10,19 @@ except FileNotFoundError:
     pc.printout('Error: file "config/credentials.ini" not found!\n', pc.RED)
     sys.exit(0)
 except Exception as e:
-    pc.printout("Error: {}".format(e), pc.RED)
-    pc.printout("\n")
+    pc.printout("Error: {}\n".format(e), pc.RED)
     sys.exit(0)
 
 def getUsername():
     try:
         return config["Credentials"]["username"]
     except KeyError:
-        pc.printout("Error: missing \"username\" field in \"config/credentials.ini\"", pc.RED)
-        pc.printout("\n")
+        pc.printout('Error: missing "username" field in "config/credentials.ini"\n', pc.RED)
         sys.exit(0)
 
 def getPassword():
     try:
         return config["Credentials"]["password"]
     except KeyError:
-        pc.printout("Error: missing \"password\" field in \"config/credentials.ini\"", pc.RED)
-        pc.printout("\n")
+        pc.printout('Error: missing "password" field in "config/credentials.ini"\n', pc.RED)
         sys.exit(0)
